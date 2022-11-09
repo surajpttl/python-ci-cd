4811eced55bced326d0e56d04d9dbeff7e275d95
surajpttl <surajp1.ttl@tatamotors.com>
2022-11-09 18:15:47 +0530

    Changes in files

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
 
-sudo chmod 777 var/lib/jenkins/workspace/python-ci-cd-testing
-#source env/bin/activate
-source barcode-env/bin/activate
-
+source env/bin/activate
+#source barcode-env/bin/activate
 echo "ENV is activated  "
-
 export DEVELOPMENT_FLAG=local
-
 echo "DEVELOPMENT_FLAG is set on Local "
-
 cd /var/lib/jenkins/workspace/python-ci-cd-testing
-
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic -- no-input
-
 echo "Migrations done"
 python3 manage.py runserver 8002
 
-cd /var/lib/jenkins/workspace/python-ci-cd-testing
-
-sudo cp -rf gunicorn.socket /etc/systemd/system/
-sudo cp -rf gunicorn.service /etc/systemd/system/
-
+#cd /var/lib/jenkins/workspace/python-ci-cd-testing
+#sudo cp -rf gunicorn.socket /etc/systemd/system/
+#sudo cp -rf gunicorn.service /etc/systemd/system/
 echo "$USER"
 echo "$PWD"
-
-
-
-
 #sudo systemctl daemon-reload
 #sudo systemctl start gunicorn
 #echo "Gunicorn has started."
