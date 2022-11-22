3f1415848ece092ae0fdcd9e2bda95bea60a0a72
surajpttl <surajp1.ttl@tatamotors.com>
2022-11-22 14:13:25 +0530

    Changes in files

--- gunicorn.sh
+++ gunicorn.sh
@@ -12,20 +12,20 @@ python3 manage.py migrate
 python3 manage.py collectstatic -- no-input
 echo "Migrations done"
 
-python3 manage.py runserver 8003
+#python3 manage.py runserver 8003
 
-#cd /var/lib/jenkins/workspace/python-ci-cd-testing
-#sudo cp -rf gunicorn.socket /etc/systemd/system/
-#sudo cp -rf gunicorn.service /etc/systemd/system/
+cd /var/lib/jenkins/workspace/python-ci-cd-testing
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
 echo "$USER"
 echo "$PWD"
-#sudo systemctl daemon-reload
-#sudo systemctl start gunicorn
-#echo "Gunicorn has started."
-#sudo systemctl enable gunicorn
-#echo "Gunicorn has been enabled."
-#sudo systemctl restart gunicorn
-#sudo systemctl status gunicorn
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+echo "Gunicorn has started."
+sudo systemctl enable gunicorn
+echo "Gunicorn has been enabled."
+sudo systemctl restart gunicorn
+sudo systemctl status gunicorn
 
 
 
